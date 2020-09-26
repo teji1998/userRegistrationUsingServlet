c2236fa8e28a3bbf355d406c40e0a6ca6a78080c
“teji1998” <tejasvinirpk@gmail.com>
2020-09-26 09:25:27 +0530

    Added:Code to display all users

--- src/main/java/com/bridgelabz/userregistration/UserServlet.java
+++ src/main/java/com/bridgelabz/userregistration/UserServlet.java
@@ -1,11 +1,14 @@
 package com.bridgelabz.userregistration;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
 
 @WebServlet("/")
 public class UserServlet extends HttpServlet {
@@ -23,5 +26,53 @@ public class UserServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         String action = request.getServletPath();
+
+        try {
+            switch (action) {
+                case "/new":
+                    showNewForm(request, response);
+                    break;
+                case "/insert":
+                    insertUser(request, response);
+                    break;
+                case "/delete":
+                    deleteUser(request, response);
+                    break;
+                case "/edit":
+                    showEditForm(request, response);
+                    break;
+                case "/update":
+                    updateUser(request, response);
+                    break;
+                default:
+                    listUser(request, response);
+                    break;
+            }
+        } catch (SQLException ex) {
+            throw new ServletException(ex);
+        }
+    }
+
+    private void showNewForm(HttpServletRequest request, HttpServletResponse response) {
+    }
+
+    private void insertUser(HttpServletRequest request, HttpServletResponse response) {
+    }
+
+    private void updateUser(HttpServletRequest request, HttpServletResponse response) {
+    }
+
+    private void showEditForm(HttpServletRequest request, HttpServletResponse response) {
+    }
+
+    private void deleteUser(HttpServletRequest request, HttpServletResponse response) {
+    }
+
+    private void listUser(HttpServletRequest request, HttpServletResponse response)
+            throws SQLException, IOException, ServletException {
+        List<User> listUser = userDao.selectAllUsers();
+        request.setAttribute("listUser", listUser);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("user-list.jsp");
+        dispatcher.forward(request, response);
     }
 }
